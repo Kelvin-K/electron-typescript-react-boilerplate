d4cc41e782cbc7177b7232601cba55474f2633de
Kamani, Kelvin <kelvin.kamani@emtecinc.com>
2018-12-24 18:48:26 +0530

    setup testing

---
+++ src/test/helloWorld.test.tsx
@@ -0,0 +1,12 @@
+import { shallow } from 'enzyme';
+import * as React from 'react';
+import { HelloWorld } from '../renderer/components/helloWorld';
+
+test("Should render", () =>
+{
+    const userName = "Kelvin"
+    const helloWorld = shallow(<HelloWorld userName={ userName } />);
+
+    expect(helloWorld).toBeTruthy();
+    expect(helloWorld.text()).toEqual(`Hello ${ userName }`);
+});
---
+++ src/test/setupEnzyme.ts
@@ -0,0 +1,3 @@
+import { configure } from 'enzyme';
+const EnzymeAdapter = require('enzyme-adapter-react-16');
+configure({ adapter: new EnzymeAdapter() });
