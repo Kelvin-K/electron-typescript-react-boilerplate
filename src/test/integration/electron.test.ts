81cd1db3d2c3f7f92ec23f9e1566e452d64bab1f
Kelvin Kamani <kamani_kelvin@yahoo.com>
2018-12-24 21:40:31 +0530

    extracted electron setup

--- src/test/integration/electron.test.ts
+++ src/test/integration/electron.test.ts
@@ -1,16 +1,4 @@
-import * as path from "path";
-import { Application } from "spectron";
-
-var electronPath = path.join(__dirname, '..', "..", "..", 'node_modules', '.bin', 'electron');
-var appPath = path.join(__dirname, '..');
-
-if (process.platform === 'win32')
-    electronPath += '.cmd';
-
-var app = new Application({
-    path: electronPath,
-    args: [ appPath ]
-});
+const app = require("../setupElectron");
 
 test("it should start", async () =>
 {
---
+++ src/test/setupElectron.ts
@@ -0,0 +1,15 @@
+import * as path from "path";
+import { Application } from "spectron";
+
+var electronPath = path.join(__dirname, '..', "..", "..", 'node_modules', '.bin', 'electron');
+var appPath = path.join(__dirname, '..');
+
+if (process.platform === 'win32')
+    electronPath += '.cmd';
+
+const app = new Application({
+    path: electronPath,
+    args: [ appPath ]
+ });
+
+export default app;
